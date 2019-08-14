687e56ba51e5ed114576fc7ea331433224762d14
Gamal Abdul Nasser <gamalanpro@gmail.com>
2019-08-14 09:55:46 +0700

    allow scan with complete wildcard

--- storageredis_test.go
+++ storageredis_test.go
@@ -110,6 +110,11 @@ func TestRedisStorage_List(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Len(t, keys, 3)
 	assert.Contains(t, keys, path.Join("acme", "example.com", "sites", "example.com", "example.com.crt"))
+
+	keys, err = rd.List("*", true)
+	assert.NoError(t, err)
+	assert.Len(t, keys, 3)
+	assert.Contains(t, keys, path.Join("acme", "example.com", "sites", "example.com", "example.com.crt"))
 }
 
 func TestRedisStorage_ListNonRecursive(t *testing.T) {
@@ -123,6 +128,12 @@ func TestRedisStorage_ListNonRecursive(t *testing.T) {
 	assert.NoError(t, err)
 
 	keys, err := rd.List(path.Join("acme", "example.com", "sites"), false)
+	assert.NoError(t, err)
+
+	assert.Len(t, keys, 1)
+	assert.Contains(t, keys, path.Join("acme", "example.com", "sites", "example.com"))
+
+	keys, err = rd.List("*", false)
 	assert.NoError(t, err)
 
 	assert.Len(t, keys, 1)
