eeb393866c31ce7b52b0f44d826cd4b2f90db2c1
Jordi Masip <jordi@masip.cat>
2020-12-08 12:20:04 +0100

    Add export command

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -11,38 +13,86 @@ import (
 	"go.uber.org/zap"
 )
 
-func migrateFile(rd *storageredis.RedisStorage, caddyFolder string, path string) {
-	rd.Logger.Infof("Migrating %s...", path)
-	binary, err := ioutil.ReadFile(caddyFolder + path)
-	if err != nil {
-		panic(err)
+func main() {
+	rd := initRedis()
+
+	importCmd := flag.NewFlagSet("import", flag.ExitOnError)
+	exportCmd := flag.NewFlagSet("export", flag.ExitOnError)
+	source := importCmd.String("source", "", "path to the Caddy Data folder (Required)")
+	dest := exportCmd.String("dest", ".", "path to write the exported files")
+
+	var cmd string = ""
+	if len(os.Args) >= 2 {
+		cmd = os.Args[1]
 	}
-	rd.Store(path, binary)
+
+	switch cmd {
+	case "import":
+		importCmd.Parse(os.Args[2:])
+		if *source == "" {
+			rd.Logger.Fatal("source path not specified")
+		}
+		importFiles(rd, *source)
+	case "export":
+		exportCmd.Parse(os.Args[2:])
+		exportFiles(rd, *dest)
+	default:
+		fmt.Println("expected 'import' or 'export' subcommands")
+		os.Exit(1)
+	}
 }
 
-func main() {
-	logger, _ := zap.NewProduction()
+func initRedis() *storageredis.RedisStorage {
+	config := zap.NewDevelopmentConfig()
+	config.Level = zap.NewAtomicLevelAt(zap.InfoLevel)
+	logger, _ := config.Build()
+
 	rd := storageredis.RedisStorage{
 		Logger: logger.Sugar(),
 	}
 	rd.GetConfigValue()
-	rd.BuildRedisClient()
-
-	caddyFolder := os.Getenv("CADDY_FOLDER")
-	if caddyFolder == "" {
-		panic("CADDY_FOLDER not defined")
+	err := rd.BuildRedisClient()
+	if err != nil {
+		rd.Logger.Fatal(err)
 	}
+	return &rd
+}
 
-	err := filepath.Walk(caddyFolder, func(path string, f os.FileInfo, err error) error {
+func importFiles(rd *storageredis.RedisStorage, caddyFolder string) error {
+	err := filepath.Walk(caddyFolder, func(fullpath string, f os.FileInfo, err error) error {
 		if err != nil {
-			panic(err)
+			rd.Logger.Fatal(err)
 		}
 		if !f.IsDir() {
-			migrateFile(&rd, caddyFolder, strings.TrimPrefix(path, caddyFolder))
+			path := strings.TrimPrefix(fullpath, caddyFolder)
+			rd.Logger.Infof("Importing %s...", path)
+			binary, err := ioutil.ReadFile(fullpath)
+			if err != nil {
+				rd.Logger.Fatal(err)
+			}
+			rd.Store(path, binary)
 		}
 		return nil
 	})
+	return err
+}
+
+func exportFiles(rd *storageredis.RedisStorage, dest string) {
+	keys, err := rd.List("*", true)
 	if err != nil {
-		panic(err)
+		rd.Logger.Fatal(err)
+	}
+	for _, key := range keys {
+		rd.Logger.Infof("Exporting %s...", key)
+		val, err := rd.Load(key)
+		if err != nil {
+			rd.Logger.Fatal(err)
+		}
+		path := filepath.Join(dest, key)
+		err = os.MkdirAll(filepath.Dir(path), 0700)
+		if err != nil {
+			rd.Logger.Fatal(err)
+		}
+		ioutil.WriteFile(path, val, 0600)
 	}
 }
